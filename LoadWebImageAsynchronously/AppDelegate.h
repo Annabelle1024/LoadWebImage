5ceb52aeb62304f72872142aaae16adf8e493093
Annabelle <annabelle1121@sina.com>
2016-05-28 19:44:45 +0800

    Initial Commit

---
+++ LoadWebImageAsynchronously/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LoadWebImageAsynchronously
+//
+//  Created by Annabelle on 16/5/28.
+//  Copyright © 2016年 annabelle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LoadWebImageAsynchronously/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LoadWebImageAsynchronously
+//
+//  Created by Annabelle on 16/5/28.
+//  Copyright © 2016年 annabelle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
